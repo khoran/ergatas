5dccb4b31b0d36d5479147d0d63b8a407ef780f1
Kevin Horan <kevinhoran@globalrecordings.net>
2020-12-10 17:11:03 -0800

    possible fix for attachement issue

--- server.js
+++ server.js
@@ -103,7 +103,7 @@ app.use(serveStatic(path.join(__dirname, 'public'), {
 
 
 app.use(express.json({limit: '50mb'}));
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({extended:true,limit: "5mb"}));
 
 
 
