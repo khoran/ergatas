a4e07bf523cd37c874c8f0e4a0038f363e1a9ef5
Kevin Horan <kevinhoran@globalrecordings.net>
2026-03-12 16:24:01 -0400

    set managed profile card to fixed size for loading

--- sql/migrations/add_posts_table.sql
+++ sql/migrations/add_posts_table.sql
@@ -8,18 +8,3 @@ CREATE TABLE IF NOT EXISTS web.posts(
 
 ALTER TABLE web.posts OWNER TO ergatas_dev;
 ALTER TABLE web.posts ENABLE ROW LEVEL SECURITY;
-GRANT SELECT, INSERT, UPDATE, DELETE ON web.posts TO ergatas_view_owner;
-GRANT USAGE, SELECT ON SEQUENCE web.posts_post_key_seq TO ergatas_view_owner;
-
-INSERT INTO web.posts(missionary_profile_key, date_added, prayer_count, data)
-SELECT mp.missionary_profile_key,
-       coalesce((post->>'date_added')::date, current_date),
-       coalesce((post->>'prayer_count')::int, 0),
-       coalesce((post - 'date_added' - 'prayer_count'),'{}'::jsonb)
-FROM web.missionary_profiles as mp,
-     jsonb_array_elements(coalesce(mp.data->'posts','[]'::jsonb)) as post
-;
-
-UPDATE web.missionary_profiles
-   SET data = data - 'posts'
- WHERE data ? 'posts';
